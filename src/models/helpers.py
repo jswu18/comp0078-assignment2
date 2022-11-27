44339fce37df5c689940364846a9887877191275
flxclxc <pisacane.alexei@gmail.com>
2022-11-27 20:32:00 +0000

    make adjacency matrix variable size 2

--- src/models/helpers.py
+++ src/models/helpers.py
@@ -6,7 +6,7 @@ def make_graph_matrix(X):
     n = X.shape[0]
     graph = tree.query(X, k = 4)[1][:,1:]
     W = np.zeros((n,n))
-    for i in range(800):
+    for i in range(n):
         W[i,graph[i,:]] = 1
         W[graph[i,:],i] = 1
     return W
