6f9154ecd85292c5e3a51a8cfb590b4bdd9d1cd4
flxclxc <pisacane.alexei@gmail.com>
2022-11-27 20:29:43 +0000

    graph matrix edit

--- src/models/helpers.py
+++ src/models/helpers.py
@@ -1,8 +1,8 @@
 from sklearn.neighbors import KDTree
+import numpy as np
 
 def make_graph_matrix(X):
     tree = KDTree(X)
-    graph = {idx : None for idx in range(X.shape[0])}
     graph = tree.query(X, k = 4)[1][:,1:]
     W = np.zeros((800,800))
     for i in range(800):
