f61e81719dd2f448d87b30e1cbaf34b569f0556d
flxclxc <pisacane.alexei@gmail.com>
2022-11-27 20:31:07 +0000

    make adjacency matrix variable size

--- src/models/helpers.py
+++ src/models/helpers.py
@@ -3,8 +3,9 @@ import numpy as np
 
 def make_graph_matrix(X):
     tree = KDTree(X)
+    n = X.shape[0]
     graph = tree.query(X, k = 4)[1][:,1:]
-    W = np.zeros((800,800))
+    W = np.zeros((n,n))
     for i in range(800):
         W[i,graph[i,:]] = 1
         W[graph[i,:],i] = 1
